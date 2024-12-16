6de5a8079fb3ac493bb60b5916860b54b4064519
RomarioKavin1 <romario7kavin@gmail.com>
2024-12-17 01:21:53 +0530

    mainpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 "use client";
+import BlockBuilder from "@/components/blockBuilder";
 import PortfolioSidebar from "@/components/metaSidebar";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen">
       <PortfolioSidebar />
-      <main className="flex-1 p-8">
-        <h1 className="text-2xl font-bold">Content</h1>
+      <main className="flex-1">
+        <h1 className="">
+          <BlockBuilder />
+        </h1>
       </main>
     </div>
   );
